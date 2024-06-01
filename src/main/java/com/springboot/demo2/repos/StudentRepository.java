fb1bc0bffb3d3205ddee61e5398cec5778301d6d
DeveloperSpiritTeam-95 <developerspirit95@gmail.com>
2024-06-01 12:44:22 +0530

    demo2 added

--- src/main/java/com/springboot/demo2/repos/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo2.repos;
-
-import com.springboot.demo2.entity.StudentData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<StudentData,String> {
-}
